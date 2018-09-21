dc3d882e041d9fc987944cd1acb63d3c0cb3d034
Jeff Davies <jdavies@ncsu.edu>
2018-09-20 22:26:52 -0400

    testing made compare equal to assignments in sample.js

--- network/lib/sample.js
+++ network/lib/sample.js
@@ -49,7 +49,7 @@ function CreateOrder(purchase) {
  * @transaction
  */
 function Buy(purchase) {
-    if (purchase.order.status == JSON.stringify(orderStatus.Created))
+    if (purchase.order.status = JSON.stringify(orderStatus.Created))
     {
         purchase.order.buyer = purchase.buyer;
         purchase.order.seller = purchase.seller;
@@ -64,7 +64,7 @@ function Buy(purchase) {
  * @transaction
  */
 function OrderCancel(purchase) {
-    if ((purchase.order.status == JSON.stringify(orderStatus.Created)) || (purchase.order.status == JSON.stringify(orderStatus.Bought)))
+    if ((purchase.order.status = JSON.stringify(orderStatus.Created)) || (purchase.order.status = JSON.stringify(orderStatus.Bought)))
     {
         purchase.order.buyer = purchase.buyer;
         purchase.order.seller = purchase.seller;
